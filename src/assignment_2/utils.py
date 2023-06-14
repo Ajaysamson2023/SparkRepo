7348e67f570a5216faea52ba1549210ca3b26fe8
Ajaysamson2023 <ajay.k@diggibyte.com>
2023-06-14 16:25:22 +0530

    This is utils file updated for assignment_2

--- src/assignment_2/utils.py
+++ src/assignment_2/utils.py
@@ -27,7 +27,7 @@ def count_lines(df):
 
 
 def find_warn(df):
-    find_warn_log = df.filter(df.event_processing == "WARN").count()
+    find_warn_log = df.filter(col("event_processing").like("%WARN%")).agg(count("*").alias("Warn_message"))
     return find_warn_log
 
 
@@ -37,17 +37,31 @@ def api_client(df):
 
 
 def split_column(df):
-    split_col = df.withColumn('id', split(col('api_client'), '--').getItem(0)) \
+    split_col = df.withColumn('client_id', split(col('api_client'), '--').getItem(0)) \
         .withColumn('client_data', split(col('api_client'), '--').getItem(1)) \
         .withColumn('retriever', split(col('client_data'), ':').getItem(0)) \
         .withColumn('ghtorrent', split(col('client_data'), ':').getItem(1))
     return split_col
 
 
-# def client_request(split_column):
-#     client_requests = split_column.groupBy("id", "client_data").count()
-#     return client_requests
+def client_request(split_col):
+    client_requests = split_col.groupBy("client_id").agg(count("*").alias("max_client_request"))
+    max_requests = client_requests.orderBy(col("max_client_request").desc())
+    max_request = max_requests.limit(1)
+    return max_request
 
-# def fail_req(split_col):
-#     fail_request = split_col.count()
-#     return fail_request
+
+def failed_request(split_col):
+    failed_req = split_col.filter(col("ghtorrent").like("%Failed%"))
+    max_failed = failed_req.groupBy(col("client_id")).agg(count("client_id").alias("Client_id_failed"))
+    failed_http = max_failed.orderBy(col("Client_id_failed").desc())
+    fail_request = failed_http.limit(1)
+    return fail_request
+
+
+def most_active_repo(split_col):
+    most_active = split_col.groupBy("retriever").agg(count("*").alias("gh_torrent_rb_data"))
+    most_active_repository = most_active.orderBy(col("gh_torrent_rb_data").desc())
+    most_active_repository = most_active_repository.limit(1)
+    return most_active_repository
+
